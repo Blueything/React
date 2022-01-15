e83b63bfcf9eb921b16a24f58ef256a727442f88
Blueything <48907750+Blueything@users.noreply.github.com>
2022-01-15 21:39:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  const name="hardik"; 
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
